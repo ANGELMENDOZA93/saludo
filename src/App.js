4b24581374375a4ad719bc0ff728efeccbfd41df
unknown <dotadoc@hotmail.com>
2022-04-27 15:17:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <dvi>
+        <img src="https://scontent.fbaq1-1.fna.fbcdn.net/v/t39.30808-6/249756268_1312202265960435_6060613897582096121_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=174925&_nc_eui2=AeHfOE3FGmbt6RzM07aTLOTuoXTex2Yuy0GhdN7HZi7LQUbeNJ-HuenBUdgCLW_85yw&_nc_ohc=TAVFZBsxqtQAX8_BeVp&_nc_ht=scontent.fbaq1-1.fna&oh=00_AT-tt1IC-cKoM_PWlLZtjYzKookqBxFdE3CHXr3hl9Tl4Q&oe=626BCBB5" alt="PARA UNA ROSA" />
+           <div className="encima" >OTRA ROSA</div>
+      </dvi>
+      <div>  
+        <img class="top" src="https://www.nationalgeographic.com.es/medio/2022/01/10/el-efecto-petalo-de-rosa-es-el-modo-en-que-las-esfericas-gotitas-de-rocio-se-adhieren-a-los-petalos_d39a1bcd_1200x630.jpg" alt="OTRA ROSA"/>
+      </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Para una Rosa Otra Rosa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
